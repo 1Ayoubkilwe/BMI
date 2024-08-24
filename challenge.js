5fd4dc1787cf227b20dce6e81818b0345b51474b
Ayoub Jama Khalid <buuyacamaajdilaakh@gmail.com>
2024-08-24 11:44:47 +0300

    my respond

--- challenge.js
+++ challenge.js
@@ -1,2 +1,19 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function calculateBMI(weight, height) {
+    return weight / (height * height);
+}
+
+const aliBMI = calculateBMI(70, 1.75);
+const mohamedBMI = calculateBMI(85, 1.80);
+
+console.log(`Ali's BMI: ${aliBMI}`);
+console.log(`Mohamed's BMI: ${mohamedBMI}`);
+
+if (aliBMI > mohamedBMI) {
+    console.log("Ali has a higher BMI");
+} else if (aliBMI < mohamedBMI) {
+    console.log("Mohamed has a higher BMI");
+} else {
+    console.log("Ali and Mohamed have the same BMI");
+}
